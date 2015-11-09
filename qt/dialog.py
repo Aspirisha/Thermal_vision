121fe26828b674563e20b2951adc5ebaf1646b59
Andy <andy.silv@yandex.ru>
2015-11-09 21:06:10 +0300

    not very good

--- qt/dialog.py
+++ qt/dialog.py
@@ -11,16 +11,31 @@ from PySide import QtCore, QtGui
 from dialog_ui import Ui_Dialog
 import sys
 
+def show_all_widgets_in_layout(layout, show):
+    items = (layout.itemAt(i) for i in range(layout.count())) 
+    for item in items:
+        if item.widget() is not None:
+            item.widget().hide()
+        if item.layout():
+            show_all_widgets_in_layout(item.layout(), show)
+
 class ControlDialog(QtGui.QDialog):
     def __init__(self, parent=None):
         super(ControlDialog, self).__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        show_all_widgets_in_layout(self.ui.by_name_options_layout, False)
+        self.ui.gridLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+        #self.ui.gridLayout.adjustSize()
 
     def ok_pressed():
         pass
 
-qtapp = QtGui.QApplication(sys.argv)
-dlg = ControlDialog()
-dlg.show()
-qtapp.exec_()
+def main():
+    qtapp = QtGui.QApplication(sys.argv)
+    dlg = ControlDialog()
+    dlg.show()
+    qtapp.exec_()
+
+if __name__ == '__main__':
+    main()
